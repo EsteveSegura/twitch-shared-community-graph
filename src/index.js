df453691d0783f20de9ad6b893e2979ed012ff11
Esteve <estevesegura@protonmail.com>
2021-09-08 21:35:53 +0200

    :family: Add one more streamer to list

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import {adjacencyList} from './domain/services/adjacencyList';
 import Graph from './domain/graph/graph';
 
 async function drawGraph() {
-  const streamers = [await getAllFollowers('95005780'), await getAllFollowers('143003858')];
-  const createAdjacencyList = adjacencyList( streamers);
+  const streamers = [
+    await getAllFollowers('148382191'),
+    await getAllFollowers('95005780'),
+    await getAllFollowers('143003858'),
+  ];
+  const createAdjacencyList = adjacencyList(streamers);
   const streamsGraph = new Graph({title: 'Streamers', htmlId: 'sigma-container'});
   streamsGraph.bulkAdjacencyList(createAdjacencyList);
   streamsGraph.render();
